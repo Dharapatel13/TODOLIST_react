5c86f682b5770ef116bf736abe92cbc71b87f729
Dharapatel13 <https://github.com/Dharapatel13/To-Do-List.git>
2021-04-14 11:23:47 +0530

    working

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import TODO from './add';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+   <>
+   <div className="container-fluid ">
+<p className="h2 text-center p-3 mt-4 text-info">TODO MANAGER</p>
+   <TODO/>
+
+   </div>
+   </>
+  );
+}
+
+export default App;
